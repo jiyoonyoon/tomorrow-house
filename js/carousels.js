55a1ac17f9ed9d38e58510127ca34453250675f8
jiyonnyonn <mam7631@gmail.com>
2024-02-19 22:25:22 +0900

    🚧  Separate .user-gallery: mobile / desktop

--- js/carousels.js
+++ js/carousels.js
@@ -11,24 +11,29 @@ const productCarousel = tns({
   preventScrollOnTouch: 'auto'
 });
 
-const userGallery = tns({
-  container: '.user-gallery .slider-list',
+const userGalleryMobile = tns({
+  container: '.user-gallery.is-mobile .slider-list',
   gutter: 4,
   edgePadding: 16,
   loop: false,
   controls: false,
-  controlsContainer: '.user-gallery-controls',
-  navContainer: '.user-gallery .thumbnail-list',
+  navContainer: '.user-gallery.is-mobile .thumbnail-list',
   navAsThumbnails: true,
   arrowKeys: true,
   mouseDrag: true,
   preventScrollOnTouch: 'auto',
-  responsive: {
-    768: {
-      gutter: 6,
-      edgePadding: 75,
-      controls: true,
+})
 
-    }
-  }
+const userGalleryDesktop = tns({
+  container: '.user-gallery.is-desktop .slider-list',
+  gutter: 6,
+  edgePadding: 75,
+  loop: false,
+  controls: true,
+  controlsContainer: '.user-gallery.is-desktop .user-gallery-controls',
+  navContainer: '.user-gallery.is-desktop .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: 'auto',
 })
