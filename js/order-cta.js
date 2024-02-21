5b4c2ea04ff53041ed1522cc278babc3c7727932
jiyonnyonn <mam7631@gmail.com>
2024-02-21 23:05:00 +0900

    🚧  Implement toggling bookmark button

--- js/order-cta.js
+++ js/order-cta.js
@@ -16,3 +16,26 @@ function closeOrderForm() {
 }
 
 orderFormOverlay.addEventListener('click', closeOrderForm)
+
+function toggleOderCtaBookmark() {
+  const [icon, countSpen] = this.children //[아이콘, 카운트 spen]
+  const count = Number(countSpen.innerHTML.replaceAll(',', ''))
+
+  let newCount = count
+  if (this.classList.contains('is-active')) {
+    //NOTE : 앞으로 비활성화 // - 1
+    icon.classList.add('ic-bookmark')
+    icon.classList.remove('ic-bookmark-filled')
+    newCount = newCount - 1
+  } else {
+    //NOTE : 앞으로 활성화 // + 1
+    icon.classList.add('ic-bookmark-filled')
+    icon.classList.remove('ic-bookmark')
+    newCount = newCount + 1
+  }
+
+  countSpen.innerHTML = newCount.toLocaleString()
+  this.classList.toggle('is-active')
+}
+
+orderCtaBookmarkButton.addEventListener('click', toggleOderCtaBookmark)
